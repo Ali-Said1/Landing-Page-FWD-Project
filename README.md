6962422424e399dc2b59410fdc70425584a287f8
Ali-Said1 <asazizg1@gmail.com>
2021-09-09 16:01:28 +0200

    Added function to js Edited Readme file

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
 # Landing Page FWD Project
 
-## Project 1 (Landing Page) for FWD professional program
+ Project 1 (Landing Page) for FWD professional program
 
-#### This project contains a navbar automatically generated using js 
+## Overview:
 
-#### The sections get an active class when they get close to the top of the viewport to distinguish the section in view
+- ### This project contains a navbar automatically generated using js 
 
-#### using only js when you click on a "li" the page scrolls towards that section
+The navbar `li` are Generated according to the number of sections with flexibility that the more sections you add the more navbar `li` are added...
 
-#### a scroll to top button appears when you scroll down the page and disappears when you scroll up again
+---
 
-#### Th navbar dissapears automatically after 5 seconds if the user stops scrolling
+- ### The sections get an active class when they get close to the top of the viewport to distinguish the section in view
+
+Also the navbar `li` related to this section gains an active class...
+
+---
+
+- ### using only js when you click on a "li" the page scrolls towards that section
+
+It uses the `ScrollTo` JavaScript method...
+
+---
+
+
+- ### Scroll to top 
+
+a scroll to top button appears when you scroll down the page and disappears when you scroll up again...
+
+---
+
+- ### The navbar dissapears automatically after 5 seconds if the user stops scrolling
+
+It uses timeout to detect that the user is no longer scrolling...
+
+---
+
+## Author:
+
+**Ali Said**
--- js/app.js
+++ js/app.js
@@ -11,18 +11,20 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 const sections = document.querySelectorAll('section');
 const navUl = document.getElementById('navbar__list');
+let navbarListItems = [];
 const headerElem = document.getElementsByClassName('page__header')[0];
 let fragment = document.createDocumentFragment();
 let dataAttrVal;
 const goUp = document.getElementsByClassName('arrowUp')[0];
+let hideNavbar;
 /**
  * End Global Variables
  * Start Helper Functions
@@ -30,9 +32,14 @@ const goUp = document.getElementsByClassName('arrowUp')[0];
  */
 
 
-    function removeActive(){
+    function removeActiveFormSections(){
         sections.forEach(function(element){
             element.classList.remove('your-active-class')
+        });
+    };
+    function removeActiveFromListItems(){
+        navbarListItems.forEach(function(liElem){
+            liElem.classList.remove('active')
         });
     };
 
@@ -49,26 +56,41 @@ sections.forEach(function(section){
     const li = document.createElement("li");
     dataAttrVal = section.getAttribute('data-nav');
     li.innerHTML = dataAttrVal;
-    li.setAttribute('id', dataAttrVal.split(" ")[0].toLowerCase()+dataAttrVal.split(" ")[1]);
+    li.classList.add(dataAttrVal.split(" ")[0].toLowerCase()+dataAttrVal.split(" ")[1]);
     li.classList.add('menu__link');
     fragment.appendChild(li);
+    fragment.firstElementChild.classList.add('active');
+    navbarListItems.push(li);
 })
 
 navUl.appendChild(fragment)
 // Add class 'active' to section when near top of viewport
-function ActiveClass(){
+function addActiveClassToSections(){
     sections.forEach(function(elem){
         if(elem.getBoundingClientRect().top <= elem.getBoundingClientRect().height / 2 && elem.getBoundingClientRect().bottom > elem.getBoundingClientRect().height / 2){
-            removeActive();
+            removeActiveFormSections();
             elem.classList.add('your-active-class');
         }
     })}
+
+
+    // Add class 'active' to corresponding navbar item when section near top of viewport
+    function addActiveClassToNavbar(){
+        navbarListItems.forEach(function(liElem){
+            sections.forEach(function(section){
+                // checks if the navber list item class list contains the section id
+                if(liElem.classList.contains(section.getAttribute('id')) && section.classList.contains('your-active-class')){
+                    removeActiveFromListItems();
+                    liElem.classList.add('active');
+                };
+                });
+        });
+    };
 
 // Scroll to anchor ID using scrollTO event
 scrollToSection();
 
 // Hide fixed navigation bar while not scrolling 
-let hideNavbar;
 
 document.addEventListener('scroll', function(){
     clearTimeout(hideNavbar)
@@ -100,14 +122,17 @@ function scrollToTop(){
 function scrollToSection(){
     navUl.addEventListener('click', function(e){
         sections.forEach(function(section){
-            if(section.getAttribute('id') === e.target.getAttribute("id")){
+            if(e.target.classList.contains(section.getAttribute('id'))){
                 scrollTo({left: 0, top: section.offsetTop, behavior :'smooth'});
             }
         })
     })
 }
 // Set sections as active
-document.addEventListener('scroll', ActiveClass);
+document.addEventListener('scroll', addActiveClassToSections);
+
+// Set navbar list items as active
+document.addEventListener('scroll', addActiveClassToNavbar)
 //scroll to top
 document.addEventListener('scroll', scrollToTop)
 
